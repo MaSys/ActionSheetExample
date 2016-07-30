3d64f07d693fd454dd805669fd097979297603f6
Yaser Almasri <yaser@masys.co>
2016-07-29 22:26:22 -0500

    add action sheet menu

--- ActtionSheetExample/ViewController.swift
+++ ActtionSheetExample/ViewController.swift
@@ -20,6 +20,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showActionSheet(sender: AnyObject) {
+        let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            print("File Saved")
+        })
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .Destructive, handler: {
+            (alert: UIAlertAction!) -> Void in
+            print("File Deleted")
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+        })
+        
+        optionMenu.addAction(saveAction)
+        optionMenu.addAction(deleteAction)
+        optionMenu.addAction(cancelAction)
+
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+    }
 
 }
 
